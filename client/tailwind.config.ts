a994d1276098582e4d92b19b714af9054fd5bb09
rakshithujwal11 <rakshithujwal11@gmail.com>
2025-07-08 00:13:20 +0530

    Updated Tailwind Config

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,6 +9,30 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        while: "#ffffff",
+        gray: {
+          100: "#f5f5f5",
+          200: "#e5e5e5",
+          300: "#d4d4d4",
+          400: "#a3a3a3",
+          500: "#737373",
+          600: "#525252",
+          700: "#404040",
+          800: "#262626",
+          900: "#171717",
+        },
+        blue: {
+          200: "#93c5fd",
+          400: "#60a5fa",
+          500: "#3b82f6",
+        },
+        "dark-bg": "#101214",
+        "dark-secondary": "#1d1f21",
+        "dark-tertiary": "#3b3d40",
+        "blue-primary": "#0275ff",
+        "stroke-dark": "#2d3135",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
